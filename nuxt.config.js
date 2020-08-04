bcf6bfb8e62463afbde55a139cfce7bcdf098750
Roman <felixcon@gmail.com>
2020-08-04 23:26:41 +0300

    yandex

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,8 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' },
+      { name:"yandex-verification", content: "6b9d1ddb8e384195" }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
