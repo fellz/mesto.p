6c2c23cebdf1263ce7daebb3c05bbdd871d15ffe
Roman <felixcon@gmail.com>
2020-08-04 23:50:43 +0300

    mesto

--- components/common/Menu.vue
+++ components/common/Menu.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <span class="navbar-brand">Mesto platform</span>
+    <span class="navbar-brand">Mesto.platform</span>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
