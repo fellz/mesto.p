002e465b719b086a6a4e750869197f166ab5b6b8
Roman <felixcon@gmail.com>
2020-08-21 17:46:00 +0300

    login fix

--- pages/connect/google/redirect.vue
+++ pages/connect/google/redirect.vue
@@ -6,21 +6,21 @@
 export default {
   async created(){
     const user = await this.$axios.$get(`/auth/google/callback?access_token=${this.$route.query.id_token}`)
-    console.log(user)
-    // // set user in store with jwt and user
-    // this.$store.commit('SET_USER', user)
+    //console.log(user)
+    // set user in store with jwt and user
+    this.$store.commit('SET_USER', user)
     
-    // const profile = resp.user.profile;
-    // // if profile undefined --> create profile
-    // if(!profile){
-    //   await this.createProfile(resp.user.id)
-    //   this.setCookieToken(resp)
-    // }else{
-    //   // else get profile and set it store and redirect
-    //   const profile = await this.$axios.$get(`/profiles/${resp.user.profile.id}`)
-    //   this.$store.dispatch("setProfile", {profile});
-    //   this.setCookieToken(resp)
-    // }
+    const profile = resp.user.profile;
+    // if profile undefined --> create profile
+    if(!profile){
+      await this.createProfile(resp.user.id)
+      this.setCookieToken(resp)
+    }else{
+      // else get profile and set it store and redirect
+      const profile = await this.$axios.$get(`/profiles/${resp.user.profile.id}`)
+      this.$store.dispatch("setProfile", {profile});
+      this.setCookieToken(resp)
+    }
   },
   methods:{
     setCookieToken(resp){
