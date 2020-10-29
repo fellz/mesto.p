d3cec166ed836409fa202fd1e9353db3355ab2d3
Yakovlev Roman <felixcon@gmail.com>
2020-10-29 14:18:39 +0300

    adaptive fix

--- components/projects/index.vue
+++ components/projects/index.vue
@@ -1,11 +1,17 @@
 <template>
-  <div>
-    <h3 v-if="loading">Загружаемся....</h3>
-    <div v-for="project of projects" :key="project.id">
-      <short-project :project="project"   />
-    </div>
-    <pagination @new-start-number="setStartPage($event)" :all_items="all_projects" :resource="resource"></pagination>
-  </div>
+    <v-row justify="center">
+        <v-col xm="12" sm="9" md="7">
+            <h3 v-if="loading">Загружаемся....</h3>
+            <div v-for="project of projects" :key="project.id">
+                <short-project :project="project" />
+            </div>
+            <pagination
+                @new-start-number="setStartPage($event)"
+                :all_items="all_projects"
+                :resource="resource"
+            ></pagination>
+        </v-col>
+    </v-row>
 </template>
 
 <script>
@@ -13,37 +19,41 @@ import ShortProject from "~/components/projects/short_project/index.vue";
 import Pagination from "~/components/common/pagination.vue";
 
 export default {
-  name: "ProjectsList",
-  data() {
-    return {
-      projects: [],
-      all_projects: 0,
-      start: 0,
-      resource: "projects",
-      baseUrl: process.env.baseUrl,
-      loading: false
-    };
-  },
-  components: {
-    ShortProject,
-    Pagination
-  },
-  async created() {
-    this.loading = true
-    await this.getProjects(this.start)
-    await this.getAllProjects()
-    this.loading = false
-  },
-  methods:{
-     async getProjects(start){
-      this.projects = await this.$axios.$get(`/projects?_start=${start}&_limit=5&_sort=created_at:DESC`);
+    name: "ProjectsList",
+    data() {
+        return {
+            projects: [],
+            all_projects: 0,
+            start: 0,
+            resource: "projects",
+            baseUrl: process.env.baseUrl,
+            loading: false,
+        };
     },
-    async getAllProjects(){
-      this.all_projects = await this.$axios.$get(`${this.baseUrl}/projects/count`);
+    components: {
+        ShortProject,
+        Pagination,
     },
-    setStartPage(new_start_number){
-      this.getProjects(new_start_number)
+    async created() {
+        this.loading = true;
+        await this.getProjects(this.start);
+        await this.getAllProjects();
+        this.loading = false;
     },
-  }
+    methods: {
+        async getProjects(start) {
+            this.projects = await this.$axios.$get(
+                `/projects?_start=${start}&_limit=5&_sort=created_at:DESC`
+            );
+        },
+        async getAllProjects() {
+            this.all_projects = await this.$axios.$get(
+                `${this.baseUrl}/projects/count`
+            );
+        },
+        setStartPage(new_start_number) {
+            this.getProjects(new_start_number);
+        },
+    },
 };
 </script>
